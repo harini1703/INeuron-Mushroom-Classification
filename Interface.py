34534ec9ed701c95b880c9ec86e71cbf50422cae
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2022-09-06 17:33:31 +0530

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -89,7 +89,7 @@ if nav=='Dataset':
   st.header("About the Dataset:")
   st.write("Although this dataset was originally contributed to the UCI Machine Learning repository nearly 30 years ago, mushroom hunting is enjoying new peaks in popularity.")
   st.write("This dataset includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms in the Agaricus and Lepiota Family Mushroom drawn from The Audubon Society Field Guide to North American Mushrooms (1981). Each species is identified as definitely edible, definitely poisonous, or of unknown edibility and not recommended. This latter class was combined with the poisonous one. The Guide clearly states that there is no simple rule for determining the edibility of a mushroom; no rule like leaflets three, let it be for Poisonous Oak and Ivy.")
-  dataset=pd.read_csv("mushrooms(1).csv")
+  dataset=pd.read_csv("mushrooms.csv")
   if st.checkbox("Show the data"):
     st.dataframe(dataset)
     
